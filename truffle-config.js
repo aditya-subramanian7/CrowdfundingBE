32f06801c5878c679b57e74745a7f69464ffa021
Aditya Subramanian <adu.subr@gmail.com>
2022-03-15 16:09:06 +0530

    Changed package to its proper state and changed truffle-config.js to ganache local

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,28 @@
 const path = require("path");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Mnemonic = "wait essay vast quarter address second marble stand mutual puzzle shuffle congress";
+const AccountIndex = 0;
 
 module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545,
-      gasPrice: 10000000000,
-      gas: 6721975
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "5777"
+    },
+    ganache_local:{
+      provider: function(){
+        return new HDWalletProvider(Mnemonic,"http://127.0.0.1:7545",AccountIndex)
+      },
+      network_id: "5777"
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.6.1"
     }
   }
 };
